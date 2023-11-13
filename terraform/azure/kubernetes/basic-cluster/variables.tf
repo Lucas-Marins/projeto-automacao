5e5f080db13c3a914e4a09f476e6a90ef6ab570e
Lucas-Marins <lmarins02@hotmail.com.br>
2023-11-13 13:53:39 -0300

    modified:   playbooks/azure-deploy-kubernetes.yaml

--- terraform/azure/kubernetes/basic-cluster/variables.tf
+++ terraform/azure/kubernetes/basic-cluster/variables.tf
@@ -3,8 +3,10 @@
 
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  default = "example-aks"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "West Europe"
 }
